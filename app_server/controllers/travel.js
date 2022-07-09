8fc00e6323953fa58a410d4ca9f33b49ef00c781
Mark Holden <mark.holden@monscierge.com>
2022-07-09 12:30:50 -0500

    use json resource for trip content

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,6 +1,9 @@
+const fs = require('fs');
+const trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+
 /* GET travel page. */
 const travel = function(req, res, next) {
-    res.render('travel', { title: 'Travlr Getaways' });
+    res.render('travel', { title: 'Travlr Getaways', trips: trips });
 };
 
 module.exports = {
