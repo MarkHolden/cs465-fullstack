ec3bf4c8e9f34031e3f95de7496b695d7ea9e73d
Mark Holden <mark.holden@monscierge.com>
2022-07-09 12:38:27 -0500

    convert index to handlebar page

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+// TODO: use a service to retrieve trips rather than a json file.
 const trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
 
 /* GET travel page. */
