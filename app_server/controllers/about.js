c695d40597307719f67aaa360f034404b43f88fa
Mark Holden <mark.holden@monscierge.com>
2022-07-11 00:04:58 -0500

    get about info from data

--- app_server/controllers/about.js
+++ app_server/controllers/about.js
@@ -1,5 +1,9 @@
+const fs = require('fs');
+// TODO: use a service to retrieve trips rather than a json file.
+const aboutUs = JSON.parse(fs.readFileSync('./data/about.json', 'utf8'));
+
 const about = function(req, res, next) {
-    res.render('about', { title: 'Travlr Getaways' });
+    res.render('about', { title: 'Travlr Getaways', about: aboutUs });
 };
 
 module.exports = {
