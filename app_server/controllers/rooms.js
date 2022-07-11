4a19fe187180333750bb4573afa4fa46c5187243
Mark Holden <mark.holden@monscierge.com>
2022-07-10 20:58:17 -0500

    get rooms data from json

--- app_server/controllers/rooms.js
+++ app_server/controllers/rooms.js
@@ -1,5 +1,9 @@
+const fs = require('fs');
+// TODO: use a service to retrieve trips rather than a json file.
+const roomData = JSON.parse(fs.readFileSync('./data/rooms.json', 'utf8'));
+
 const rooms = function(req, res, next) {
-    res.render('rooms', { title: 'Travlr Getaways' });
+    res.render('rooms', { title: 'Travlr Getaways', rooms: roomData });
 };
 
 module.exports = {
