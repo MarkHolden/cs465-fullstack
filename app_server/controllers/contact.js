3497197aad53992d755e63e8af2efd91102c9841
Mark Holden <mark.holden@monscierge.com>
2022-07-11 00:05:09 -0500

    get contact info from data

--- app_server/controllers/contact.js
+++ app_server/controllers/contact.js
@@ -1,5 +1,9 @@
+const fs = require('fs');
+// TODO: use a service to retrieve trips rather than a json file.
+const contactInfo = JSON.parse(fs.readFileSync('./data/contact.json', 'utf8'));
+
 const contact = function(req, res, next) {
-    res.render('contact', { title: 'Travlr Getaways' });
+    res.render('contact', { title: 'Travlr Getaways', contact: contactInfo });
 };
 
 module.exports = {
