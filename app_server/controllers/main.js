8a1d88f528d60625a5abe2a9622fad69b224a586
Mark Holden <mark.holden@monscierge.com>
2022-07-10 23:35:46 -0500

    get testimonials from data

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -2,9 +2,15 @@ const fs = require('fs');
 // TODO: use a service to retrieve trips rather than a json file.
 const featured_links = JSON.parse(fs.readFileSync('./data/featured-links.json', 'utf8'));
 const blogs = JSON.parse(fs.readFileSync('./data/blogs.json', 'utf8'));
+const testimonials = JSON.parse(fs.readFileSync('./data/testimonials.json', 'utf8'));
 
 const index = function(req, res, next) {
-    res.render('index', { title: 'Travlr Getaways', featured_links: featured_links, blogs: blogs });
+    res.render('index', { 
+        title: 'Travlr Getaways', 
+        featured_links: featured_links, 
+        blogs: blogs, 
+        testimonials: testimonials
+    });
 };
 
 module.exports = {
