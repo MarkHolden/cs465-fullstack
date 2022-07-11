ea43e295f932f77b39ce58d16b68d92466c01c13
Mark Holden <mark.holden@monscierge.com>
2022-07-10 22:38:09 -0500

    move news to json data

--- app_server/controllers/news.js
+++ app_server/controllers/news.js
@@ -1,5 +1,9 @@
+const fs = require('fs');
+// TODO: use a service to retrieve trips rather than a json file.
+const newsData = JSON.parse(fs.readFileSync('./data/news.json', 'utf8'));
+
 const news = function(req, res, next) {
-    res.render('news', { title: 'Travlr Getaways' });
+    res.render('news', { title: 'Travlr Getaways', news: newsData });
 };
 
 module.exports = {
