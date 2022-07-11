a2993151ab80efe468a9e508f32f26ecc0de18b6
Mark Holden <mark.holden@monscierge.com>
2022-07-10 23:50:46 -0500

    get home data from data

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -3,13 +3,15 @@ const fs = require('fs');
 const featured_links = JSON.parse(fs.readFileSync('./data/featured-links.json', 'utf8'));
 const blogs = JSON.parse(fs.readFileSync('./data/blogs.json', 'utf8'));
 const testimonials = JSON.parse(fs.readFileSync('./data/testimonials.json', 'utf8'));
+const homeData = JSON.parse(fs.readFileSync('./data/home.json'));
 
 const index = function(req, res, next) {
     res.render('index', { 
         title: 'Travlr Getaways', 
         featured_links: featured_links, 
         blogs: blogs, 
-        testimonials: testimonials
+        testimonials: testimonials,
+        home: homeData,
     });
 };
 
