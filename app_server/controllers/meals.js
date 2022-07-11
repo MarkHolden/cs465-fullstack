f42357ee78eaba79085619649018f4c9b799ee7f
Mark Holden <mark.holden@monscierge.com>
2022-07-10 20:39:05 -0500

    get meals from json

--- app_server/controllers/meals.js
+++ app_server/controllers/meals.js
@@ -1,5 +1,9 @@
+const fs = require('fs');
+// TODO: use a service to retrieve trips rather than a json file.
+const mealData = JSON.parse(fs.readFileSync('./data/meals.json', 'utf8'));
+
 const meals = function(req, res, next) {
-    res.render('meals', { title: 'Travlr Getaways' });
+    res.render('meals', { title: 'Travlr Getaways', meals: mealData });
 };
 
 module.exports = {
