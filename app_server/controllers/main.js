6765c7ea4bdfd9656422c3923d2cf9241ab97f55
Mark Holden <mark.holden@monscierge.com>
2022-07-10 23:22:59 -0500

    get blogs and featured links from data

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,5 +1,10 @@
+const fs = require('fs');
+// TODO: use a service to retrieve trips rather than a json file.
+const featured_links = JSON.parse(fs.readFileSync('./data/featured-links.json', 'utf8'));
+const blogs = JSON.parse(fs.readFileSync('./data/blogs.json', 'utf8'));
+
 const index = function(req, res, next) {
-    res.render('index', { title: 'Travlr Getaways' });
+    res.render('index', { title: 'Travlr Getaways', featured_links: featured_links, blogs: blogs });
 };
 
 module.exports = {
