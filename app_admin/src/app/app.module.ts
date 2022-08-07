8c725116b93ffde125cc04bf23117e45cf82cf45
Mark Holden <mark.holden@monscierge.com>
2022-08-07 02:09:50 -0500

    add trip card component

--- app_admin/src/app/app.module.ts
+++ app_admin/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { TripListingComponent } from './trip-listing/trip-listing.component';
+import { TripCardComponent } from './trip-card/trip-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TripListingComponent
+    TripListingComponent,
+    TripCardComponent
   ],
   imports: [
     BrowserModule
