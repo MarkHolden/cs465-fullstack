4088439659e21e7912bf48913e49ede28d4c2eb8
Mark Holden <mark.holden@monscierge.com>
2022-08-07 11:38:00 -0500

    Stub out endpoints for create, update, and delete trips

--- app_api/controllers/trips.js
+++ app_api/controllers/trips.js
@@ -35,7 +35,86 @@ const get = async (req, res) => {
         });
 };
 
+// POST: /trips Creates a trip.
+const create = async (req, res) => {
+    console.log(req);
+    console.log(req.body);
+    // model.create(req.body)
+    //     .exec((err, trips) => {
+    //         if (!trips) {
+    //             return res.status(404)
+    //                 .json({ "message": "no trips found." });
+    //         } else if (err) {
+    //             return res.status(500)
+    //                 .json(err);
+    //         } else {
+    //             return res.status(201)
+    //                 .json(trips);
+    //         }
+    //     });
+};
+
+// PUT: /trips/:tripId Updates a trip.
+const update = async (req, res) => {
+    console.log(req);
+    console.log(req.body);
+    model.find({ 'code': req.params.tripId })
+        .exec((err, trips) => {
+            if (!trips) {
+                return res.status(404)
+                    .json({ "message": "no trips found." });
+            } else if (err) {
+                return res.status(500)
+                    .json(err);
+            } else {
+                model.updateOne({ 'code': req.params.tripId }, req.body)
+                    .exec((err, trip) => {
+                        if (err) {
+                            return res.status(500)
+                                .json(err);
+                        } else {
+                            return res.status(200)
+                                .json(trip);
+                        }
+                    });
+            }
+        });
+    // model.create(req.body)
+    //     .exec((err, trips) => {
+    //         if (!trips) {
+    //             return res.status(404)
+    //                 .json({ "message": "no trips found." });
+    //         } else if (err) {
+    //             return res.status(500)
+    //                 .json(err);
+    //         } else {
+    //             return res.status(200)
+    //                 .json(trips);
+    //         }
+    //     });
+};
+
+// GET: /trips/:tripId - get trip by id.
+const remove = async (req, res) => {
+    model.deleteOne({ 'code': req.params.tripId })
+        .exec((err, trips) => {
+            if (!trips) {
+                return res.status(404)
+                    .json({ "message": "no trips found." });
+            } else if (err) {
+                return res.status(500)
+                    .json(err);
+            } else {
+                return res.status(200) // TODO: Maybe 204 here
+                    .json(trips);
+            }
+        });
+};
+
 module.exports = {
     list,
     get,
+    create,
+    update,
+    remove,
 };
--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -4,5 +4,8 @@ const tripsController = require('../controllers/trips');
 
 router.get('/trips', tripsController.list);
 router.get('/trips/:tripId', tripsController.get);
+router.post('/trips', tripsController.create);
+router.put('/trips/:tripId', tripsController.update);
+router.delete('/trips/:tripId', tripsController.remove);
 
 module.exports = router;
